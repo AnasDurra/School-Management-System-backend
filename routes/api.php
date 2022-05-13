becf169d3f683b7b7bf61a71dae707d5435dc515
gabsonia_ANAS <anas.reish.77@gmail.com>
2022-05-13 23:20:03 +0300

    fix

--- routes/api.php
+++ routes/api.php
@@ -27,6 +27,7 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 
 Route::middleware('auth:sanctum')->group(function () {
+    
     Route::get('/me', [AuthController::class, 'me']);
     Route::post('/logout', [AuthController::class, 'logout']);
 
