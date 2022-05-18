11998eaaf2a64b18221a6c3a7bd92a22464b01bd
AnasDr <me9e98.2012@gmail.com>
2022-05-18 21:26:23 +0300

    fix pt2 http://127.0.0.1:8000/api/classrooms/all

--- app/Http/Controllers/ClassroomController.php
+++ app/Http/Controllers/ClassroomController.php
@@ -37,6 +37,9 @@ class ClassroomController extends Controller
                         $teacher_subject = teacher_subject::query()
                             ->where('id', '=', $classroom_teacherSubject_in_classroom[$j]->teacherSubject_id)->first();
                         $subject=Subject::query()->where('id','=',$teacher_subject->subject_id)->first();
+                        $teacher_user=Teacher::query()->where('user_id','=',$teacher_subject->teacher_id)->first();
+                        $classroom_teacherSubject_in_classroom[$j]->teacher_id = $teacher_subject->teacher_id;
+                        $classroom_teacherSubject_in_classroom[$j]->teacher_user = $teacher_user;
                         $classroom_teacherSubject_in_classroom[$j]->subject_name = $subject->name;
                     }
                 $classrooms[$i]->teacher_subject = $classroom_teacherSubject_in_classroom;
