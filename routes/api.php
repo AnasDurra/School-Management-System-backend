c722282f2895b1b6a234ac00f70cfeb3ba5f519b
gabsonia_ANAS <anas.reish.77@gmail.com>
2022-05-20 13:36:30 +0300

    logout

--- routes/api.php
+++ routes/api.php
@@ -25,11 +25,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
-
+Route::post('/logout', [AuthController::class, 'logout']);
 Route::middleware('auth:sanctum')->group(function () {
 
     Route::get('/me', [AuthController::class, 'me']);
-    Route::post('/logout', [AuthController::class, 'logout']);
+
 
 
     // Route::group(['middleware' => 'isAdmin'], function () {
